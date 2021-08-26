6a6bda4dda213de8c33831fffb969069179b5250
h62446244! <pepesi258@hanmail.net>
2021-08-26 17:27:58 +0900

    [승강장간격분석] * 승강장 노선 Path 그려지는 코드 추가

--- js/custom.js
+++ js/custom.js
@@ -59,6 +59,123 @@ $(function(){
     } 
     checkIe();
 
+    
+    function makePath(line){
+        var line = line || 1;
+        console.log(line + "호선 그리기")
+        var st_data = new Array; 
+        lineData.forEach(function(v,i,a){
+            if(v.line == line && v.safe == "X"){
+                st_data.push(v.station)
+            }
+        });
+        console.log(st_data);
+        $(".path-draw-canvas #path-start-decoo .number-box p").html(line);
+        $(".path-draw-canvas #path-start-deco").removeClass();
+        $(".path-draw-canvas #path-start-deco").addClass("deco0"+line)
+        var svgWidth = (isMobile)? screenWidth: 1000;
+        var svgHeight = 500; 
+        var svgId = "PATH"
+        $("#"+svgId).css({"width":svgWidth, "height":svgHeight});
+        $("#"+svgId).html("");
+        var curveWidth = 150; 
+        var curveHeight = 50;
+        var stationMargin = 100;
+        var horizonWidth = svgWidth - (curveWidth*2);
+        var stationMaxCount = horizonWidth / stationMargin;
+
+        var _path = new String;
+        var pathStart= "M 0 0 Q 0 "+curveHeight+" 150 "+curveHeight
+        _path = pathStart + " L 850 "+curveHeight; //첫번째줄 직선
+        _path = _path + " Q 1000 "+curveHeight+" 1000 "+ curveHeight*2; //첫번째줄 커브 
+        //M 0 0 Q 0 100 150 100 L 700 100 Q 800 100 800 200 
+
+        var floorLength = parseInt(st_data.length / stationMaxCount);        
+        console.log(st_data.length, floorLength);
+        if( floorLength > 0 ){
+            console.log(floorLength+"개의 라인 그려야함");
+            if( floorLength >= 1 ){
+                _path = _path +  " Q 1000 "+curveHeight*3+" 850 "+ curveHeight*3
+                _path = _path +  " L 150 "+curveHeight*3
+            }
+            if( floorLength >= 2){
+                _path = _path +  " Q 0 "+curveHeight*3+" 0 "+ curveHeight*4
+                _path = _path +  " Q 0 "+curveHeight*5+" 150 "+curveHeight*5
+                _path = _path +  " L 850 " + curveHeight *5
+            }
+            if( floorLength >= 3){
+               _path = _path +  " Q 1000 "+curveHeight*5+" 1000 "+ curveHeight*6
+               _path = _path +  " Q 1000 "+curveHeight*7+" 850 "+curveHeight*7
+               _path = _path +  " L 150 " + curveHeight *7
+            } 
+            if( floorLength >= 4){
+               _path = _path +  " Q 0 "+curveHeight*7+" 0 "+ curveHeight*8
+               _path = _path +  " Q 0 "+curveHeight*9+" 150 "+curveHeight*9
+               _path = _path +  " L 850 " + curveHeight *9
+            }
+        }else{
+            console.log("한줄 끝");
+        } 
+       console.log(_path)
+
+        var tempPathObj = document.getElementById(svgId).appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+        tempPathObj.setAttributeNS(null,"d", _path);
+        tempPathObj.setAttributeNS(null,"class", "path pathAni path-line0"+line+" path-hide-f"+floorLength);
+        
+        
+        var hideLength = 1200;
+        var animatePath = document.querySelector(".pathAni")
+        var animatePathLength = hideLength*(floorLength+1);
+        animatePath.style.strokeDasharray = animatePathLength;
+        animatePath.style.strokeDashoffset = animatePathLength;
+
+        var duration = 70*(floorLength+1);
+        var elapsed = 0;
+        animate(elapsed);
+        function animate(offset) {
+            setTimeout(function(){
+                elapsed++;
+                if (elapsed > duration){ 
+                    //elapsed = 0;
+                    console.log("stop");
+                }else{
+                    animate((elapsed / duration) * animatePathLength);
+                }  
+            }, 10)
+            animatePath.style.strokeDashoffset = animatePathLength-offset;
+            //console.log(offset);
+        }
+        
+        var makeXcor = function(i) {
+            return (i % stationMaxCount) * 100;
+        };
+        var makeYcor = function(i) {
+            return parseInt(i / stationMaxCount) *curveHeight*2
+        };
+
+        var station_holder= document.getElementById(svgId).appendChild(document.createElementNS("http://www.w3.org/2000/svg", "g"));
+        station_holder.setAttributeNS(null,"class", "station-holder");
+        station_holder.setAttributeNS(null,"transform", "translate(150, 50)");
+        for(d=0;d<st_data.length;d++){
+             var tempGroup =  station_holder.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "g"));
+             tempGroup.setAttributeNS(null,"class", "station-group st-i-"+d+" st-line-"+line);
+             tempGroup.setAttributeNS(null,"transform", "translate("+makeXcor(d)+","+makeYcor(d)+")");
+             var tempCircle = tempGroup.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "circle"));
+             tempCircle.setAttributeNS(null,"class", "station-circle");
+             var templabel= tempGroup.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "text"));
+             templabel.innerHTML = st_data[d];
+        }
+
+     
+
+    }
+    //makePath();
+    
+    $("#METRO_BTN ul li").on("click", function(){
+        var userC_line = $(this).attr("data-id");     
+        userC_line = userC_line.replace("s_", ""); 
+        makePath(userC_line);
+    });
 
 	function init(){
 		
---
+++ js/metroLineData.js
@@ -0,0 +1,1482 @@
+var lineData = [
+  {
+    "line": 1,
+    "station": "동대문",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "동묘앞",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "서울역",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "시청",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "신설동",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "제기동",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "종각",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "종로3가",
+    "safe": "X"
+  },
+  {
+    "line": 1,
+    "station": "종로5가",
+    "safe": "O"
+  },
+  {
+    "line": 1,
+    "station": "청량리",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "강남",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "강변",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "건대입구",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "교대",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "구로디지털단지",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "구의",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "낙성대",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "당산",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "대림",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "도림천",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "동대문역사문화공원",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "뚝섬",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "문래",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "방배",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "봉천",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "사당",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "삼성",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "상왕십리",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "서울대입구",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "서초",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "선릉",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "성수",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "시청",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신답",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신당",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신대방",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신도림",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신림",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신설동",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "신정네거리",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "신촌",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "아현",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "양천구청",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "역삼",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "영등포구청",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "왕십리",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "용답",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "용두",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "을지로3가",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "을지로4가",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "을지로입구",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "이대",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "잠실",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "잠실나루",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "잠실새내",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "종합운동장",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "충정로",
+    "safe": "X"
+  },
+  {
+    "line": 2,
+    "station": "한양대",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "합정",
+    "safe": "O"
+  },
+  {
+    "line": 2,
+    "station": "홍대입구",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "가락시장",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "경복궁",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "경찰병원",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "고속터미널",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "교대",
+    "safe": "O"
+  },
+  {
+    "line": 3,
+    "station": "구파발",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "금호",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "남부터미널",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "녹번",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "대청",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "대치",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "도곡",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "독립문",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "동대입구",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "매봉",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "무악재",
+    "safe": "O"
+  },
+  {
+    "line": 3,
+    "station": "불광",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "수서",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "신사",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "안국",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "압구정",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "약수",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "양재",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "연신내",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "오금",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "옥수",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "을지로3가",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "일원",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "잠원",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "종로3가",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "지축",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "충무로",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "학여울",
+    "safe": "X"
+  },
+  {
+    "line": 3,
+    "station": "홍제",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "길음",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "남태령",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "노원",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "당고개",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "동대문",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "동대문역사문화공원",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "동작",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "명동",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "미아",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "미아사거리",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "사당",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "삼각지",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "상계",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "서울역",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "성신여대입구",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "수유",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "숙대입구",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "신용산",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "쌍문",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "이촌",
+    "safe": "O"
+  },
+  {
+    "line": 4,
+    "station": "창동",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "총신대입구",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "충무로",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "한성대입구",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "혜화",
+    "safe": "X"
+  },
+  {
+    "line": 4,
+    "station": "회현",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "강동",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "강일",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "개롱",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "개화산",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "거여",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "고덕",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "공덕",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "광나루",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "광화문",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "군자",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "굽은다리",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "길동",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "김포공항",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "까치산",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "답십리",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "동대문역사문화공원",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "둔촌동",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "마곡",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "마장",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "마천",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "마포",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "명일",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "목동",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "미사",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "발산",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "방이",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "방화",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "상일동",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "서대문",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "송정",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "신금호",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "신길",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "신정",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "아차산",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "애오개",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "양평",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "여의나루",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "여의도",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "영등포구청",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "영등포시장",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "오금",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "오목교",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "올림픽공원",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "왕십리",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "우장산",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "을지로4가",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "장한평",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "종로3가",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "천호",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "청구",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "충정로",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "하남검단산",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "하남시청",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "하남풍산",
+    "safe": "X"
+  },
+  {
+    "line": 5,
+    "station": "행당",
+    "safe": "O"
+  },
+  {
+    "line": 5,
+    "station": "화곡",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "고려대",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "공덕",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "광흥창",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "구산",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "녹사평",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "대흥",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "독바위",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "돌곶이",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "동묘앞",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "디지털미디어시티",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "마포구청",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "망원",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "버티고개",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "보문",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "봉화산",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "불광",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "삼각지",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "상수",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "상월곡",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "새절",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "석계",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "신내",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "신당",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "안암",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "약수",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "역촌",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "연신내",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "월곡",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "월드컵경기장",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "응암",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "이태원",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "증산",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "창신",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "청구",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "태릉입구",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "한강진",
+    "safe": "O"
+  },
+  {
+    "line": 6,
+    "station": "합정",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "화랑대",
+    "safe": "X"
+  },
+  {
+    "line": 6,
+    "station": "효창공원앞",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "가산디지털단지",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "강남구청",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "건대입구",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "고속터미널",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "공릉",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "광명사거리",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "군자",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "굴포천",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "까치울",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "남구로",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "남성",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "내방",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "노원",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "논현",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "대림",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "도봉산",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "뚝섬유원지",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "마들",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "먹골",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "면목",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "반포",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "보라매",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "부천시청",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "부천종합운동장",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "부평구청",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "사가정",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "삼산체육관",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "상도",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "상동",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "상봉",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "수락산",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "숭실대입구",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "신대방삼거리",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "신중동",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "신풍",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "어린이대공원",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "온수",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "용마산",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "이수",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "장승배기",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "장암",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "중계",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "중곡",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "중화",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "천왕",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "철산",
+    "safe": "X"
+  },
+  {
+    "line": 7,
+    "station": "청담",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "춘의",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "태릉입구",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "하계",
+    "safe": "O"
+  },
+  {
+    "line": 7,
+    "station": "학동",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "가락시장",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "강동구청",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "남한산성입구",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "단대오거리",
+    "safe": "X"
+  },
+  {
+    "line": 8,
+    "station": "모란",
+    "safe": "X"
+  },
+  {
+    "line": 8,
+    "station": "몽촌토성",
+    "safe": "X"
+  },
+  {
+    "line": 8,
+    "station": "문정",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "복정",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "산성",
+    "safe": "X"
+  },
+  {
+    "line": 8,
+    "station": "석촌",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "송파",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "수진",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "신흥",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "암사",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "잠실",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "장지",
+    "safe": "O"
+  },
+  {
+    "line": 8,
+    "station": "천호",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "둔촌오륜역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "봉은사역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "삼성중앙역",
+    "safe": "X"
+  },
+  {
+    "line": 9,
+    "station": "삼전역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "석촌고분역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "석촌역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "선정릉역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "송파나루역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "언주역",
+    "safe": "X"
+  },
+  {
+    "line": 9,
+    "station": "올림픽공원역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "종합운동장역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "중앙보훈병원역",
+    "safe": "O"
+  },
+  {
+    "line": 9,
+    "station": "한성백제역",
+    "safe": "O"
+  }
+];
