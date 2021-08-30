ab94b1321f4e05cbaf7a708b4bcb5f5b17bb4369
h62446244! <pepesi258@hanmail.net>
2021-08-30 17:05:27 +0900

    [승강장간격분석] *개별역 간격 path 그려지는 것 코드 보강, 스타일 추가 *호선별 지하철 역 path 그려지는 영역 코드 추가

--- js/custom.js
+++ js/custom.js
@@ -72,7 +72,7 @@ $(function(){
             }
         });
         console.log(st_data);
-        $(".path-draw-canvas #path-start-decoo .number-box p").html(line);
+        $(".path-draw-canvas .number-box p").html(line);
         $(".path-draw-canvas #path-start-deco").removeClass();
         $(".path-draw-canvas #path-start-deco").addClass("deco0"+line)
         var svgWidth = (isMobile)? screenWidth: 1000;
@@ -166,6 +166,7 @@ $(function(){
              tempCircle.setAttributeNS(null,"class", "station-circle");
              var templabel= tempGroup.appendChild(document.createElementNS("http://www.w3.org/2000/svg", "text"));
              templabel.innerHTML = st_data[d];
+			 templabel.setAttributeNS(null,"transform", "translate(0, -12)");
         }
 
      
@@ -188,20 +189,41 @@ $(function(){
 		$(".title-falling img").animate({"bottom":"0", "opacity":"1"}, 800, "easeOutBounce");
 	});
 
+	var signPath = "M17.1,16.3c-0.7-1.2-1.4-2.4-2.1-3.6c-0.3-0.5-0.7-0.7-1.3-0.7c-1.7,0-3.4,0-5.2,0c-0.1,0-0.2,0-0.3,0c0-0.6,0-1.1,0-1.7c0.1,0,0.2,0,0.2,0c1.2,0,2.4,0,3.8,0c0.5,0,0.8-0.2,1-0.7c0.2-0.7-0.2-1.4-1-1.4c-1.3,0-2.6,0-3.9,0c-0.1,0-0.2,0-0.2,0C8,7.8,8,7.4,7.9,7C8,6.4,8,5.8,7.7,5.2C7.3,4.8,6.9,4.5,6.5,4.3C6.7,4.2,6.9,4.2,7.1,4C7.8,3.5,8,2.7,7.9,1.9C7.8,1.5,7.6,1.1,7.3,0.7C6.9,0.3,6.5,0.2,6,0C5.8,0,5.6,0,5.4,0C5.1,0.1,5,0.2,4.8,0.2c-0.7,0.3-1.2,1-1.4,1.7c-0.1,0.5,0,1.1,0.3,1.5c0.3,0.5,0.7,0.8,1.2,1C4,4.8,3.6,5.7,3.7,6.7c0,0.6,0.1,1.1,0.1,1.6C3.3,8.6,2.9,8.9,2.6,9.2c0,0,0,0-0.1,0.1c-1.1,1.1-2,2.4-2.3,4c-0.1,0.4-0.2,0.9-0.2,1.4c0,0.5,0,0.9,0,1.3c0.1,0.5,0.2,0.9,0.2,1.4c0.4,1.6,1.2,3,2.4,4.2c0.8,0.7,1.7,1.3,2.8,1.7c0.7,0.2,1.5,0.4,2.3,0.5h0.1c0.4,0,0.9,0,1.3,0c0.2-0.1,0.5-0.1,0.8-0.2c1.1-0.2,2-0.6,2.9-1.1c0.1-0.1,0.2-0.2,0.3-0.2c1.1-0.8,2-1.8,2.7-3c0.2,0.4,0.5,0.8,0.7,1.2c0.3,0.7,1.2,0.8,1.9,0.5c0.3-0.2,0.4-0.5,0.6-0.8c0-0.2,0-0.3,0-0.6c-0.1-0.1-0.1-0.2-0.2-0.2C18.3,18.3,17.7,17.3,17.1,16.3z M14.1,18.8c-0.1,0.2-0.2,0.3-0.3,0.4c-0.6,0.8-1.3,1.5-2.1,2c-0.5,0.3-1.1,0.5-1.7,0.7c-1.2,0.3-2.4,0.2-3.7-0.1c-1.1-0.3-2.1-1-2.9-1.9C2.6,19,2.1,18.1,1.8,17c-0.4-1.6-0.2-3,0.4-4.4c0.3-0.8,0.9-1.5,1.5-2l0,0v0.1c0.1,1,0.1,1.9,0.2,2.9c0,0.4,0.2,0.8,0.5,1.1c0.5,0.6,1.1,0.8,1.9,0.8c2.3,0,4.6,0,6.9,0c0.2,0,0.4,0.1,0.5,0.2c0.3,0.5,0.6,1.1,0.9,1.6c0.1,0.1,0.1,0.2,0.2,0.2C14.6,17.9,14.4,18.4,14.1,18.8z";
+
     $(".station-name").on("click", function(){
         $(".crevasse").remove();
         var stationName = $(this).text();
+		var svgWidth = 1000;
+		$("#CREVASSE").css({"width":svgWidth});
+        var defaultLine = "M 0 -1 L "+svgWidth+" -1z";
+        var defaultPathObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+        defaultPathObj.setAttributeNS(null,"d", defaultLine);
+        defaultPathObj.setAttributeNS(null,"class", "default-line");
+		
+		var defaultTextObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "text"));
+		defaultTextObj.innerHTML = "열차 위치";
+		defaultTextObj.setAttributeNS(null,"transform", "translate(-30,0)");
+		defaultTextObj.setAttributeNS(null, "class", "defalut-text");
+		
+		var descPathObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+		descPathObj.setAttributeNS(null,"d", "M 0 100 L 1000 100z");
+		descPathObj.setAttributeNS(null,"class", "desc-path");
 
-        var defaultLine = "M 0 5 L 1000 5z";
-        var tempPathObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
-        tempPathObj.setAttributeNS(null,"d", defaultLine);
-        tempPathObj.setAttributeNS(null,"class", "crevasse");
+		var descTextObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "text"));
+		descTextObj.innerHTML = "100mm";
+		descTextObj.setAttributeNS(null,"transform", "translate(-30,105)");
+		descTextObj.setAttributeNS(null, "class", "desc-text");
 
         var st_info = new Array; 
         station_info.forEach(function(v,i,a){
             var tmpPath = "";
+			var sang;
             if(v.line == keepLine && v.station == stationName) {
                 var count = Object.keys(v["entrances_up"]).length;
+				//console.log(count);
+				sang = svgWidth/(count-1);
+				console.log(sang);
                 var k = 0;
                 for (var key in v["entrances_up"]) { 
                     // console.log("key : " + key +", value : " + v["entrances_up"][key]);
@@ -209,13 +231,35 @@ $(function(){
                     if(k==0){
                         tmpPath = "M 0 " + v["entrances_up"][key]["distance(mm)"];
                     } else {
-                        tmpPath += " L " + 50*k + " " + v["entrances_up"][key]["distance(mm)"];
+                        tmpPath += " L " + sang*k + " " + v["entrances_up"][key]["distance(mm)"];
                     }
-                    k++;
+					//if( key.slice(-1) == "1" || key.slice(-1) == "3" ){
+					if( key.slice(-1) == "1"){
+						var tempTextObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "text"));
+						tempTextObj.innerHTML = key;
+						tempTextObj.setAttributeNS(null,"transform", "translate("+ sang*k+","+(v["entrances_up"][key]["distance(mm)"]+15)+")");
+						tempTextObj.setAttributeNS(null, "class", "platform-number");
+					}
+					
+					if( v["entrances_up"][key]["wheel_enter"] =="*"){
+						var signLogoObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+						signLogoObj.setAttributeNS(null,"d", signPath);
+						signLogoObj.setAttributeNS(null,"transform", "translate("+ (sang*k-8)+",-37)");
+						signLogoObj.setAttributeNS(null, "class", "sign");
+
+						var signPathObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
+						signPathObj.setAttributeNS(null,"d", "M "+sang*k+" 0 L "+sang*k+" "+v["entrances_up"][key]["distance(mm)"]);
+						signPathObj.setAttributeNS(null, "class", "signPath");
+					}
+
+					k++;
+					
+			
                 }
                 var tempPathObj = document.getElementById("CREVASSE").appendChild(document.createElementNS("http://www.w3.org/2000/svg", "path"));
                 tempPathObj.setAttributeNS(null,"d", tmpPath);
-                tempPathObj.setAttributeNS(null,"class", "crevasse");
+                tempPathObj.setAttributeNS(null,"class", "crevasse crevasse-line0"+keepLine);
+
             } else {
                 tmpPath ="";
             }
@@ -223,6 +267,10 @@ $(function(){
         });
         
     });
+	$(".station-group").on("click", function(){
+	
+	
+	});
     
 });
 
